bc5921c3df027054fc2244fcc579b265ce329908
LilitDunamalyan11 <lilitdunamalyan93@gmail.com>
2025-07-31 13:51:58 +0400

    Add files via upload

---
+++ Myfirstprogram1.py
@@ -0,0 +1,26 @@
+# from datetime import date
+
+name = "Lilit"
+surname = "Dunamalyan"
+date_of_birth_year = 1993
+
+# Get the current year from today's date
+current_year = date.today().year
+age = current_year - date_of_birth_year
+
+print(name)
+print(surname)
+print(age)
+
+
+name = "Lilit"
+surname = "Dunamalyan"
+date_of_birth_year = 1993
+
+# Get the current year from today's date
+# current_year = date.today().year
+age = 2025 - date_of_birth_year
+
+print(name)
+print(surname)
+print(age)
